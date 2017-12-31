ce4b533bbd1d422361f554d9d7994645d0cfee1c
Yuriy Lyeshchenko <yuriylsh@live.com>
2017-12-31 16:22:50 -0600

    Adding benchmarking project; speeding up the StringExt.ToSentenceCace method ``` |                Method |     Mean |     Error |    StdDev | Scaled | ScaledSD | |---------------------- |---------:|----------:|----------:|-------:|---------:| | ToSentenceCaseInitial | 787.0 ns | 16.135 ns | 24.640 ns |   1.00 |     0.00 | |       ToSentenceCase  | 460.0 ns |  9.221 ns |  9.056 ns |   0.59 |     0.02 |

---
+++ Benchmarking/Program.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Examples.Purity;
+
+namespace Benchmarking
+{
+    class Program
+    {
+        static void Main()
+        {
+            var summary = BenchmarkRunner.Run<StringExtBenchmark>();
+        }
+    }
+
+    public class StringExtBenchmark
+    {
+        private const string input =
+            "lORem ipsUm dolor sit AMET, conSEctetur adIpisCING elit Mauris vehicula elEmenTum dolor vitae tiNCidunt.";
+
+        [Benchmark(Baseline = true)]
+        public int ToSentenceCaseInitial()
+        {
+            return input.ToSentenceCaseInitial().Length;
+        }
+
+        [Benchmark]
+        public int ToSentenceCase()
+        {
+            return input.ToSentenceCase().Length;
+        }
+    }
+}
--- Examples/Chapter02/ListFormatter/StringExt.cs
+++ Examples/Chapter02/ListFormatter/StringExt.cs
@@ -2,7 +2,10 @@ namespace Examples.Purity
 {
    public static class StringExt
    {
-      public static string ToSentenceCase(this string s)
+       public static string ToSentenceCase(this string s) 
+           => char.ToUpper(s[0]) + s.Substring(1).ToLower();
+
+       public static string ToSentenceCaseInitial(this string s)
          => s.ToUpper()[0] + s.ToLower().Substring(1);
    }
 }
